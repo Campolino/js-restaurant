ed4efb57b7e601f31ac957f65a84197e66c80763
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-11-24 05:36:19 -0300

    start configure listener for the cards

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import './style.css';
 import body from './pages/home';
 
 body();
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    const content = document.querySelector('#content');
+    content.textContent = '';
+  });
+});
